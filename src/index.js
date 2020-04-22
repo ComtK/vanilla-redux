a56ebe111c22aee55749ae01e4e68c1054fcd033
김주석 <comtk@MacBook-Pro.local>
2020-04-22 16:27:18 +0900

    state update with redux and action

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  if(action.type === 'ADD'){
+    return count + 1;
+  } else if (action.type === 'MINUS'){
+    return count - 1;
+  }else{
+    return count;
+  }
 };
 
-const store = createStore(countModifier);
+const countStore = createStore(countModifier);
 
-console.log(store.getState());
+countStore.dispatch({type: 'ADD'});
+console.log(countStore.getState());
 
 
 
