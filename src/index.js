1eb650f85a214b2e15a5e352c8d2ec6ff3cb4475
김주석 <comtk@MacBook-Pro.local>
2020-04-22 16:43:28 +0900

    update state set element

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0, action) => {
+const countModifier = (count = 0, action) => {  // <===== reducer
   if(action.type === 'ADD'){
     return count + 1;
   } else if (action.type === 'MINUS'){
@@ -16,9 +16,22 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type: 'ADD'});
-console.log(countStore.getState());
+const onChange= () => {
+  number.innerText = countStore.getState();
+}
 
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type: 'ADD'});
+}
+
+const handleMinus = () => {
+  countStore.dispatch({type: 'MINUS'});
+}
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
 
 
 
